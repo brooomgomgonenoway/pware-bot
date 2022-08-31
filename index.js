e5b172cdc00e49996396ec802533d6a611f8f80f
brooomgomgonenoway <honnabessel@gmail.com>
2022-08-31 22:39:17 +0200

    ok

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.on("messageCreate", (message) => {
         }
     }
     const split = message.content.split(" ")
-    if (typeof(split[1]) == "string"){
+    if (split.length >= 1){
         if (commands[message.content[1]]){
             commands[message.content[1]](message, split)   
         }
